a1412a6bbac17fb429651ab5245c6882e1cdc9d5
bautigonzalez <batigonza@gmail.com>
2020-07-14 15:39:57 -0300

    reducer funcionando con axios

--- src/components/addmodal/Addmodal.js
+++ src/components/addmodal/Addmodal.js
@@ -1,12 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 import { Modal, Button, Form } from "react-bootstrap"
-import { useSelector, useDispatch} from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import SearchIcon from "@material-ui/icons/Search";
 import { fetchChart } from "../../redux/action-creator/Charts"
 import "./Addmodal.scss"
 
-
 export default function addmodal(props) {
+  const [value, setValue] = useState("")
   const dispatch = useDispatch()
     return (
       <Modal
@@ -21,7 +21,7 @@ export default function addmodal(props) {
         <Modal.Body>
         <Form>
           <Form.Group className="forminput">
-            <Form.Control type="text" placeholder="Search metric" id="inputSearch"/>
+            <Form.Control onChange={(e)=>setValue(e.target.value)} value={value} type="text" placeholder="Search metric" id="inputSearch"/>
             <SearchIcon className="searchIcon"/>
           </Form.Group>
           <Form.Group className="forminput">
@@ -32,7 +32,7 @@ export default function addmodal(props) {
         <Modal.Footer id="footerModal">
           <span className="closeModal" onClick={props.onHide}>Cancelar</span>
           <span className="closeModal" onClick={()=>{
-            dispatch(fetchChart())
+            if(value) dispatch(fetchChart(value))
             props.onHide()
           }}>Listo</span>
         </Modal.Footer>
--- src/components/single/Single.js
+++ src/components/single/Single.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import Addmodal from "../addmodal/Addmodal";
 import Metric from "../metric/Metric";
+import { useSelector, useDispatch } from "react-redux"
 import "./Single.scss";
 
 export default function single() {
-  const [modalShow, setModalShow] = React.useState(false);
+  const [modalShow, setModalShow] = useState(false);
+  const charts = useSelector(store=>store.chart.charts)
+  useEffect(()=>{
+    console.log(charts)
+  })
   return (
     <div className="single">
       <Addmodal show={modalShow} onHide={() => setModalShow(false)} />
       <div className="container">
+      {charts.length > 0 ? (
+          <div className="containerMetric">
+            {charts[0].config.map(chart => <Metric key={chart.metric_id}/>)}
+          </div> 
+        ):(
          <div id="addcard">
           <h2 onClick={() => setModalShow(true)} id="add">
             +
           </h2>
           <span onClick={() => setModalShow(true)}>Add a metric</span>
         </div>
-
-        {/* <div className="containerMetric">
-          <Metric />
-        </div> */}
-      </div>
+        )}
+      </div>    
     </div>
   );
 }
