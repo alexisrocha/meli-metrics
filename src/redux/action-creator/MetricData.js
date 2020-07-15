89f414a4a3d46f0af91fa44dfefe9d72884d684f
MatiasRamosNOK <swhz05@protonmail.ch>
2020-07-15 19:51:21 -0300

    positive and negative

--- src/redux/action-creator/MetricData.js
+++ src/redux/action-creator/MetricData.js
@@ -5,20 +5,21 @@ import axios from "axios";
 let host = "https://run.mocky.io/v3/";
 
 let url = {
-    1: `decf5f46-2492-47aa-8304-c8cace4037bf`,
-    2: `c3fd409a-a428-4e86-accb-1ad29687cb76`,
-  };
+  1: `32ef81e2-abf2-496f-a7a1-229183e60e49`,
+  2: `fb224c30-4882-45a0-b74c-01ba4e51dc66`,
+};
 
-const  getMetricData = (id, metricData) => ({
-    type: GET_METRIC_DATA,
-    id,
-    metricData
+const getMetricData = (id, metricData) => ({
+  type: GET_METRIC_DATA,
+  id,
+  metricData,
 });
 
 export const fetchMetricData = (id) => {
-    return (dispatch) => {
-        axios.get(host + url[id])
-            .then(res => res.data)
-            .then(metricData => dispatch(getMetricData(id, metricData)));
-    };
-}
+  return (dispatch) => {
+    axios
+      .get(host + url[id])
+      .then((res) => res.data)
+      .then((metricData) => dispatch(getMetricData(id, metricData)));
+  };
+};
