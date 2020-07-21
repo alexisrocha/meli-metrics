f8c05519246a66be71dc3b48c1de39198a3f7230
alexisrocha <alexisleonelrocha@gmail.com>
2020-07-21 18:47:30 -0300

    boton create list

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../navbar/Navbar";
+import Addmodal from "../addmodal/Addmodal";
+
+import "./List.scss";
 
 export default function List() {
+  const [modalShow, setModalShow] = useState(false);
+
   return (
     <div>
-      <CrearLista />
-      <Listar />
+      <div className="crearLista">
+        <span>
+          <div className="crearListaContainer">
+            <b onClick={() => setModalShow(true)}> Create list</b>
+          </div>
+        </span>
+      </div>
+      <Addmodal show={modalShow} onHide={() => setModalShow(false)} />
     </div>
   );
 }
