85382091c03ffbb28e16e7503b9fb1b6d34701dd
alexisrocha <alexisleonelrocha@gmail.com>
2020-07-22 12:04:32 -0300

    debugeado lista

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Listar from "../listar/Listar";
 import { useDispatch, useSelector } from "react-redux";
 import { setLocation } from "../../redux/action-creator/Location";
