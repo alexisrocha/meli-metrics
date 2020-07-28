895c159184d97958f7158d0204f2611d95063383
alexisrocha <alexisleonelrocha@gmail.com>
2020-07-28 12:54:46 -0300

    nuevo delete en navbar versus

--- src/components/versus/Versus.js
+++ src/components/versus/Versus.js
@@ -1,6 +1,6 @@
-import React from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { Navbar, Nav } from "react-bootstrap"
+import React from "react";
+import { useDispatch, useSelector, useState } from "react-redux";
+import { Navbar, Nav } from "react-bootstrap";
 import MLA from "../../../public/flags/MLA.png";
 import MLB from "../../../public/flags/MLB.png";
 import MLC from "../../../public/flags/MLC.png";
@@ -9,34 +9,61 @@ import MLU from "../../../public/flags/MLU.png";
 import MCO from "../../../public/flags/MCO.png";
 import MGT from "../../../public/flags/MGT.png";
 import MBO from "../../../public/flags/MBO.png";
-import Avatar from '@material-ui/core/Avatar';
-import { makeStyles } from '@material-ui/core/styles';
+import Avatar from "@material-ui/core/Avatar";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import { makeStyles } from "@material-ui/core/styles";
 import "./Versus.scss";
 
-export default function versus(){
-    const selectedCountries = useSelector((store) => store.versus.selectedCountries);
-    const flags = {
-        MLA: MLA,
-        MLB: MLB,
-        MLC: MLC,
-        MLM: MLM,
-        MLU: MLU,
-        MBO: MBO,
-        MCO: MCO,
-        MGU: MGT,
-      };
+export default function versus() {
+  const selectedCountries = useSelector(
+    (store) => store.versus.selectedCountries
+  );
+  const flags = {
+    MLA: MLA,
+    MLB: MLB,
+    MLC: MLC,
+    MLM: MLM,
+    MLU: MLU,
+    MBO: MBO,
+    MCO: MCO,
+    MGU: MGT,
+  };
+  const [shadow, setShadow] = React.useState(false);
 
-    return(<>
-        <Navbar id="navbarVersus" variant="dark">
-            <div className="container">
-                <Nav id="navVersus" className="mr-auto">
-                    {selectedCountries.map((country)=><Nav.Link className="navbutton" href="#home">
-                    <img src={flags[country]} className="flagsVersus" />
-                    &nbsp;{country}
-                    </Nav.Link>)}
-                    <Nav.Link href="#home"><strong>+</strong></Nav.Link>
-                </Nav>
-            </div>
-        </Navbar>
-    </>)
+  const changeCSS = () => {
+    setShadow(true);
+  };
+
+  const changeCSSOut = () => {
+    setShadow(false);
+  };
+  return (
+    <>
+      <Navbar id="navbarVersus" variant="dark">
+        <div className="container">
+          <Nav id="navVersus" className="mr-auto">
+            {selectedCountries.map((country) => (
+              <Nav.Link
+                className="navbutton"
+                href="#home"
+                onMouseOver={changeCSS}
+                onMouseLeave={changeCSSOut}
+              >
+                <img src={flags[country]} className="flagsVersus" />
+                &nbsp;{country}
+                <DeleteIcon
+                  style={{ display: shadow ? "inline" : "none" }}
+                  className="buttonDeleteNavbar"
+                />
+              </Nav.Link>
+            ))}
+            <Nav.Link href="#home">
+              <strong>+</strong>
+            </Nav.Link>
+          </Nav>
+        </div>
+      </Navbar>
+    </>
+  );
 }
