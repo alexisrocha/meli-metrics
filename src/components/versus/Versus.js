4668ae42c9bf2c52d17d2f0765e7cfd873274db3
alexisrocha <alexisleonelrocha@gmail.com>
2020-07-31 11:02:24 -0300

    nueva efecto navbar scroll facherita eh

--- src/components/versus/Versus.js
+++ src/components/versus/Versus.js
@@ -76,7 +76,7 @@ export default function versus() {
   };
   return (
     <>
-      <Navbar id="navbarVersus" variant="dark" fixed="top">
+      <Navbar id="navbarVersus" variant="dark">
         <div className="container">
           <Nav id="navVersus" className="mr-auto">
             {selectedCountries.map((country, index) => {
