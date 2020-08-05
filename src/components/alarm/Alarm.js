84014f745670a639bd8459aa0f98e2ab2b2695bf
Hernan Hlace <hernan@hmbrokers.com>
2020-08-05 19:36:21 -0300

    alarmList

--- src/components/alarm/Alarm.js
+++ src/components/alarm/Alarm.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
 import Grid from "@material-ui/core/Grid";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMetric } from "../../redux/action-creator/Metrics";
@@ -12,24 +14,56 @@ export default function Alarm({ metricId, triggers }) {
   useEffect(() => {
     dispatch(fetchMetric(metricId));
   }, []);
-  return (
-    metric ? (
+  return metric ? (
     <>
-        <div className="triggerOptions">
-            <span>{metric.name}</span>
-            <span>Period</span>
-            <span>Site</span>
-            <span>Comparisson</span>
-        </div>
-
-        {triggers.map((trigger, index)=>
-        <div className="triggerData">
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        className="triggerOptions"
+      >
+        <Grid xs={2}></Grid>
+        <Grid xs={2}>
+          <span>{metric.name}</span>
+        </Grid>
+        <Grid xs={2} className="titles">
+          <span>Period</span>
+        </Grid>
+        <Grid xs={1} className="titles">
+          <span>Site</span>
+        </Grid>
+        <Grid xs={2} className="titles">
+          <span>Comparisson</span>
+        </Grid>
+        <Grid xs={3}></Grid>
+      </Grid>
+      {triggers.map((trigger, index) => (
+        <Grid
+          container
+          direction="row"
+          alignItems="center"
+          className="triggerData"
+        >
+          <Grid xs={2}></Grid>
+          <Grid xs={2} className="dataOption">
             <span className="triggerRegular">{"Trigger " + (index + 1)}</span>
+          </Grid>
+          <Grid xs={2} className="dataOption">
             <span>{trigger.config.dimension.site}</span>
+          </Grid>
+          <Grid xs={1} className="dataOption">
             <span>{trigger.trigger_type}</span>
+          </Grid>
+          <Grid xs={2} className="dataOption">
             <span>{trigger.config.value}</span>
-        </div>)}
+          </Grid>
+          <Grid xs={1} className="dataOption icons" >
+            <EditIcon />
+            <DeleteIcon />
+          </Grid>
+          <Grid xs={2}></Grid>
+        </Grid>
+      ))}
     </>
-    ) : (null)
-    )
+  ) : null;
 }
